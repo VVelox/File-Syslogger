b06fb827cf3ca24cab2c93a43ef211283ca3b542
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2021-11-16 08:14:46 -0600

    add initial module

---
+++ lib/File/Syslogger.pm
@@ -0,0 +1,105 @@
+package File::Syslogger;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+File::Syslogger - The great new File::Syslogger!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use File::Syslogger;
+
+    my $foo = File::Syslogger->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-file-syslogger at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=File-Syslogger>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc File::Syslogger
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=File-Syslogger>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/File-Syslogger>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/File-Syslogger>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2021 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of File::Syslogger
